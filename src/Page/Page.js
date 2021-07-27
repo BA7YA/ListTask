87f6b3f6a14133ed14bc66d7b32beb98ccdafd5e
BA7YA <iu.starodubow@yandex.ru>
2021-07-27 23:09:23 +0500

    First version of list

--- src/Page/Page.js
+++ src/Page/Page.js
@@ -1,66 +1,77 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { Tree} from 'antd';
+import axios from 'axios';
 
-function Page() {
-    return (
-        <div>
-            <Tree
-        showLine
-        switcherIcon={<DownOutlined />}
-        defaultExpandedKeys={['0-0-0']}
-        onSelect={this.onSelect}
-        treeData={[
-          {
-            title: 'parent 1',
-            key: '0-0',
-            children: [
-              {
-                title: 'parent 1-0',
-                key: '0-0-0',
-                children: [
-                  {
-                    title: 'leaf',
-                    key: '0-0-0-0',
-                  },
-                  {
-                    title: 'leaf',
-                    key: '0-0-0-1',
-                  },
-                  {
-                    title: 'leaf',
-                    key: '0-0-0-2',
-                  },
-                ],
-              },
-              {
-                title: 'parent 1-1',
-                key: '0-0-1',
-                children: [
-                  {
-                    title: 'leaf',
-                    key: '0-0-1-0',
-                  },
-                ],
-              },
-              {
-                title: 'parent 1-2',
-                key: '0-0-2',
-                children: [
-                  {
-                    title: 'leaf',
-                    key: '0-0-2-0',
-                  },
-                  {
-                    title: 'leaf',
-                    key: '0-0-2-1',
-                  },
-                ],
-              },
-            ],
-          },
-        ]}
-      />
-        </div>
+import {DownOutlined} from '@ant-design/icons';
+
+const Page = () => {
+  const [treeData, setTreeData] = useState();
+
+
+  useEffect(() => {
+    axios.get('http://164.90.161.80:3000/api/content')
+    .then(
+      r=>{
+        let data = r.data.children.map(c => { return {
+          title: c.title,
+          key: c.id,
+          icon: <DownOutlined />
+        }})
+        setTreeData(data);
+      },
+      e=>{
+        console.log(e.message);
+      }
     )
-}
+  }, [])
 
-export default Page
+  function updateTreeData(list, key, children) {
+    return list.map((node) => {
+      if (node.key === key) {
+        return { ...node, children };
+      }
+  
+      if (node.children) {
+        return { ...node, children: updateTreeData(node.children, key, children) };
+      }
+  
+      return node;
+    });
+  }
+
+  function onLoadData({ key, children }) {
+    return new Promise((resolve) => {
+      if (children) {
+        resolve();
+        return;
+      }
+
+    axios.get(`http://164.90.161.80:3000/api/content?dirId=${key}`)
+    .then(
+      r=>{
+        let data = r.data.children.map(c => { return {
+          title: c.title,
+          key: c.id,
+          isLeaf: (c.title.includes('.zip')||c.title.includes('.epub')||c.title.includes('.jpg'))? true : false,
+          
+        }});
+        setTreeData((origin) =>
+          updateTreeData(origin, key, data),
+        );
+      },
+      e=>{
+        console.log(e.message);
+      }
+    )     
+    resolve();
+    });
+  }
+
+  return <Tree loadData={onLoadData} treeData={treeData} />;
+};
+
+export default Page;
+
+
+
+
